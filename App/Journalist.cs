6cc3f75e111e75ec8be643d917e47b3cb49698b3
AhmedGamal <ahmedgamal8495@gmail.com>
2023-09-25 13:09:36 +0300

    Player A

---
+++ App/Journalist.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    public class Journalist
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        private Action<Journalist> _add;
+
+        public event Action<Journalist> AddNews
+        {
+            add { _add += value; }
+            remove { _add -= value; }
+        }
+
+        public void Add()
+        {
+            Console.WriteLine("Adding....");
+            if(_add != null)
+                _add(this);
+        }
+
+
+        
+    }
+}
